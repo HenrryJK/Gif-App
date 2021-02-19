6128661d3a0946e19f54acea695c64e5bbe287ea
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-02-19 15:49:54 -0500

    aprendiendo a usar el useEffect dentro de funciones y constantes con arreglos #6

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React , {useState , useEffect} from 'react'
 
 export const GifGrid = ({category}) => {
+    const  [count, setcount] = useState(0);
+
+    useEffect(() => { 
+        GetGifs();
+    } , [])
+
    const GetGifs = async () => {
        const url = 'https://api.giphy.com/v1/gifs/search?q=music&limit =10&api_key=4PdDJMP7JEqI0Lh3vg9XUytGVvTTuHSQ'
     
@@ -14,17 +20,16 @@ export const GifGrid = ({category}) => {
             url: img.images?.downsized_medium.url
         }
     })      
-
     console.log(gifs);
 
 
     }
-
-    GetGifs();
-   
+    // GetGifs();
     return (
         <div>
             <h3>{ category }</h3>
+            <h3>{count}</h3>
+            <button onClick={() => setcount(count + 1 )}></button>
         </div>
     )
 }
